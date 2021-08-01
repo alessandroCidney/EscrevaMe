7b68c54b264c225c379ed9f0c347b6495c93c28b
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2021-08-01 18:58:28 -0300

    Estilizações na página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,11 +4,24 @@ import { HomeHeader } from '../../components/HomeHeader';
 // SASS
 import './styles.scss';
 
+// React Router DOM
+import { useHistory } from 'react-router-dom';
+
+// Hooks
+import { useEmailAuth } from '../../hooks/useEmailAuth';
+
 // Images
 import pencilGif from '../../assets/images/animations/pencil.gif';
+import computerImg from '../../assets/images/computer/computer.png';
 
 export function Home() {
-	
+	const history = useHistory();
+
+	const { emailUser } = useEmailAuth();
+
+	if(emailUser) {
+		history.push('/login');
+	}
 
 	return (
 		<div className="home-page container-column">
@@ -20,11 +33,28 @@ export function Home() {
 
 					<div className="text">
 						Estude redação com pessoas de todo o mundo.
+
+						<input placeholder="Pesquise um tema" />
 					</div>
 				</div>
 
-				<div className="initial-info">
-					
+				<div className="initial-info dont-show">
+					<img src={computerImg} className="float-animated" alt="Computador com imagem de uma pessoa escrevendo" />
+
+					<div className="info">
+						<div className="step">
+							<div className="step-number blue">1</div>
+							<div className="step-text">Personalize seu perfil</div>
+						</div>
+						<div className="step translated">
+							<div className="step-number blue">2</div>
+							<div className="step-text">Poste suas redações</div>
+						</div>
+						<div className="step">
+							<div className="step-number blue">3</div>
+							<div className="step-text">Interaja e se desenvolva</div>
+						</div>
+					</div>
 				</div>
 			</main>
 		</div>
