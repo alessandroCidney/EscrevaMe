82faeea2b7fd375e4cd633587961c318c100caee
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2024-01-31 20:48:43 -0300

    feat: Configuring hosting and updating nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  ssr: false,
+
   build: {
     transpile: ['vuetify'],
   },
