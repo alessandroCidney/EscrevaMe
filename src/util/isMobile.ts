c637ca6ef3a4a21527fb587f23a1a2dc793ee4de
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2024-03-28 03:09:55 -0300

    feat: Started migration to the nuxt version

--- src/util/isMobile.ts
+++
@@ -1,4 +0,0 @@
-// Testa se o dispositivo é um smartphone ou um computador através da altura da tela
-export function isMobile() {
-	return window.innerWidth < 760;
-}
--- src/util/validateEmail.ts
+++
@@ -1,4 +0,0 @@
-export function validateEmail(email: string) {
-	let regExp =  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; // eslint-disable-line
-  	return regExp.test(email);
-}
